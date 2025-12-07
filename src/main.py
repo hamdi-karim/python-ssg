57144c700221d4bbb5f9a3d25207c7b8d5800df0
ThePrimeagen <k.hamdi1995@gmail.com>
2025-12-07 10:31:06 +0100

    Add main entry point

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode("My Name is", TextType.TEXT, "www.khgdwb.com")
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
