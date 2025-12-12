6e119e78f4973ec38ffa166dbca892c4eb367b1e
ThePrimeagen <k.hamdi1995@gmail.com>
2025-12-12 23:11:01 +0100

    Add new test for blocks

--- src/test_block_to_block_type.py
+++ src/test_block_to_block_type.py
@@ -43,3 +43,17 @@ class TestBlockToBlockType(unittest.TestCase):
     def test_empty_block_raises(self):
         with self.assertRaises(ValueError):
             block_to_block_type("")
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
