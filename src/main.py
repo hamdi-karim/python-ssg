26c46f81904e2f7e3838d5f36a32136b57f934e0
ThePrimeagen <k.hamdi1995@gmail.com>
2025-12-16 12:29:28 +0100

    Recusively copy then contents of static folder into the public folder

--- src/main.py
+++ src/main.py
@@ -1,9 +1,35 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    """
+    Recursively copy the contents of source_dir into destination_dir.
+    The destination directory is cleared before copying to ensure a clean state.
+    """
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
 
 
 def main():
-    text_node = TextNode("My Name is", TextType.TEXT, "www.khgdwb.com")
-    print(text_node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 if __name__ == "__main__":
